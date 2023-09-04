2f7a5fb9ec041063b419064d58342324895d35b1
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-09-04 12:41:03 +0300

    fix:adaptive category

--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -7,15 +7,15 @@ const Category = ({ params }: { params: { category: string } }) => {
     <main className="flex flex-wrap text-primary min-w-screen">
       {pizzas.map((pizza) => (
         <Link href={`/product/${pizza.id}`} key={pizza.id}
-         className='w-full sm:w-1/2 h-[60vh] lg:w-1/3 border-r-2 border-secondary border-b-2 p-2 flex flex-col justify-between group'>
+          className='w-full sm:w-1/2 h-[60vh] lg:h-[50vh] lg:w-1/3 border-r-2 border-secondary border-b-2 p-2  flex flex-col justify-between group'>
           {/* IMAGE CONTAINER */}
           {pizza.img && (
-            <div className="relative h-3/4">
-              <Image src={pizza.img} alt={pizza.title} fill className='object-contain' />
+            <div className="relative h-3/4 ">
+              <Image src={pizza.img} alt={pizza.title} fill className='object-contain' sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
             </div>
           )}
           {/* TEXT CONTAINER */}
-          <div className="flex justify-between items-center font-bold p-2 uppercase text-xl">
+          <div className="flex justify-between items-center font-bold p-2 uppercase text-xl md:text-2xl xl:text-3xl ">
             <h1 className=" flex flex-wrap">{pizza.title}</h1>
             <h2 className='group-hover:hidden'>₽{pizza.price}</h2>
             <button className='hidden group-hover:block bg-third py-2 px-4 text-white rounded-sm'>Добавить</button>
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -5,14 +5,14 @@ import { alegreya } from "../layout";
 const Menu = () => {
   return (
     <main className="min-w-screen  lg:h-[calc(100vh-8rem)] xl:h-[calc(100vh-9rem)] flex flex-col lg:center bg-gray ">
-      <div className="w-full lg:h-3/4 xl:h-full lg:w-[80%]  flex flex-col  lg:flex-row xl:min-w-full">
+      <div className="w-full lg:h-3/4  lg:w-[80%]  flex flex-col  lg:flex-row xl:min-w-full">
         {menuData.map((category) => {
           return (
             <Link
               key={category.id}
               href={`/menu/${category.slug}`}
               style={{ backgroundImage: `url(/menu/${category.slug}.jpg)` }}
-              className={`h-[calc(100vh-5rem)] lg:h-full  lg:w-1/3 center text-third   hover:text-white hover:txt-shadow-b txt-shadow-w border-y-4 border-gray  text-center`}
+              className={`h-[calc(100vh-5rem)] lg:h-full  lg:w-1/3 center text-third bg-no-repeat  hover:text-white hover:txt-shadow-b txt-shadow-w border-y-4 border-gray  text-center`}
             >
               <div className="w-full h-full backdrop-grayscale hover:backdrop-grayscale-0 transition-all duration-200 ease-in-out">
                   <h1 className="text-4xl lg:text-2xl 2xl:text-5xl font-bold uppercase mt-[45%]">
